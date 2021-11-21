e39965a2d3df479adad79f0e36c6a758d3b2e78d
suhan101 <90851339+suhan101@users.noreply.github.com>
2021-11-21 15:39:30 +0600

    registration update

---
+++ lib/Auth/registration.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class Registration extends StatefulWidget {
+  const Registration({Key? key}) : super(key: key);
+
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+        child: Column(
+
+        ),
+      ),
+    );
+  }
+}
